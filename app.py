b54a82c0cdd752cd0d3673491b915ef4f9bfa726
T D Nuwan Chathuranga <nchathu2014@gmail.com>
2021-01-19 07:10:55 +0530

    feat: complete CRUD endpoints, add ERROR handlings

--- app.py
+++ app.py
@@ -1,50 +1,170 @@
-from flask import Flask, jsonify,request,abort
+from flask import Flask, jsonify, request, abort
 from flask_migrate import Migrate
-from models import setup_db,Plant,db
+from models import setup_db, Plant, db, paginate_plants
 from flask_cors import CORS
 
 app = Flask(__name__)
-Migrate(app,db)
+Migrate(app, db)
 setup_db(app)
 CORS(app)
 
 # CORS Headers
+
+
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow_Headers','Content-Type,Authorization')
-    response.headers.add('Access-Controll-Allow-Methods','POST,PATCH,DELETE,GET,OPTIONS')
+    response.headers.add('Access-Control-Allow_Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Controll-Allow-Methods',
+                         'POST,PATCH,DELETE,GET,OPTIONS')
     return response
 
-@app.route('/plants',methods=['GET'])
-def get_all_plants():
-    # #adding pagination
-    page = request.args.get('page',1,type=int)  # if arg object page not found assign default to 1
-    start = (page-1)*10
-    end = start +10
+# GET
 
-    plants = Plant.query.all()
-    formatted_plants = [plant.format() for plant in plants]
-    print(plants)
+
+@app.route('/plants', methods=['GET'])
+def get_all_plants():
+
+    plants = Plant.query.order_by(Plant.id).all()
+    current_plants = paginate_plants(request, plants)
+
+    if len(current_plants) == 0:
+        abort(404)
+
     return jsonify({
-        'plants':formatted_plants[start:end],
-        'total_plants':len(formatted_plants),
-        'success':True
+        'plants': current_plants,
+        'total_plants': len(plants),
+        'success': True
     })
+
 
 @app.route('/plants/<int:plant_id>')
 def get_speific_plant(plant_id):
 
     #plant = Plant.query.get(plant_id)
     #plant = Plant.query.filter_by(id=plant_id).one_or_none()
-    plant = Plant.query.filter(Plant.id==plant_id).one_or_none()
+    plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
 
     if plant is None:
         abort(404)
     else:
         return jsonify({
-            'success':True,
-            'plant':plant.format()
+            'success': True,
+            'plant': plant.format()
         })
+
+
+# PATCH
+@app.route('/plants/<int:plant_id>', methods=['PATCH'])
+def update_book(plant_id):
+    body = request.get_json()
+
+    try:
+        plant = Plant.query.get(plant_id)
+
+        if plant is None:
+            abort(404)
+
+        if 'name' in body:
+            plant.name = body['name']
+
+        plant.update()
+
+        return jsonify({
+            'success': True
+        })
+
+    except:
+        abort(404)
+
+# DELETE
+
+
+@app.route('/plants/<int:plant_id>', methods=['DELETE'])
+def delete_specific_plant(plant_id):
+
+    try:
+        plant = Plant.query.get(plant_id)
+        if plant is None:
+            abort(404)
+
+        plant.delete()
+        plants = Plant.query.order_by(Plant.id).all()
+        current_plants = paginate_plants(request, plants)
+        return jsonify({
+            'success': True,
+            'deleted': plant_id,
+            'plants': current_plants,
+            'total_plants': len(Plant.query.all())
+        })
+    except:
+        abort(422)
+
+# CREATE
+
+
+@app.route('/plants', methods=['POST'])
+def create_new_plant():
+    body = request.get_json()
+
+    name = body["name"]
+    scientific_name = body["scientific_name"]
+    is_poisonous = body["is_poisonous"]
+    primary_color = body["primary_color"]
+
+    try:
+        new_plant = Plant(name=name, scientific_name=scientific_name,
+                          is_poisonous=is_poisonous, primary_color=primary_color)
+        new_plant.insert()
+        plants = Plant.query.order_by(Plant.id).all()
+        current_plants = paginate_plants(request, plants)
+
+        return jsonify({
+            "success": True,
+            "created": new_plant.id,
+            "plants": current_plants,
+            "total_plants": len(plants)
+        })
+    except:
+        abort(422)
+
+# Error Handling
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'resource not found'
+    }), 404
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        'success': False,
+        'error': 422,
+        'message': 'unprocessable'
+    }), 422
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'bad request'
+    }), 400
+
+
+@app.errorhandler(405)
+def unauthorized(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': 'method not allowed'
+    }), 405
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -5,44 +5,58 @@ password = "123"
 host = "localhost"
 database_name = "dbplants"
 
-database_path = "postgresql://{}:{}@{}/{}".format(user,password,host,database_name)
+database_path = "postgresql://{}:{}@{}/{}".format(
+    user, password, host, database_name)
 
 db = SQLAlchemy()
 
-def setup_db(app,database_path=database_path):
-        app.config['SQLALCHEMY_DATABASE_URI'] = database_path
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        db.app = app
-        db.init_app(app)
-        #db.create_all()
+
+def setup_db(app, database_path=database_path):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    # db.create_all()
+
+
+PLANTS_PER_PAGE = 8
+
+
+def paginate_plants(request, plants):
+    # adding pagination
+    # if arg object page not found assign default to 1
+    page = request.args.get('page', 1, type=int)
+    start = (page-1)*PLANTS_PER_PAGE
+    end = start + PLANTS_PER_PAGE
+
+    formatted_plants = [plant.format() for plant in plants]
+    return formatted_plants[start:end]
+
 
 class Plant(db.Model):
-        __tablename__='plants'
-        id = db.Column(db.Integer,primary_key=True)
-        name = db.Column(db.String(),nullable = False)
-        scientific_name = db.Column(db.String(),nullable=False)
-        is_poisonous  = db.Column(db.Boolean,nullable=False)
-        primary_color = db.Column(db.String(),nullable=False)
+    __tablename__ = 'plants'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    scientific_name = db.Column(db.String(), nullable=False)
+    is_poisonous = db.Column(db.Boolean, nullable=False)
+    primary_color = db.Column(db.String(), nullable=False)
 
-        def insert(self):
-                db.session.add(self)
-                db.session.commit()
-        
-        def update(self):
-                db.session.commit()
-        
-        def delete(self):
-                db.session.delete(self)
-                db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-        def format(self):
-                return{
-                        'id':self.id,
-                        'name':self.name,
-                        'scientific_name':self.scientific_name,
-                        'is_poisonous':self.is_poisonous,
-                        'primary_color':self.primary_color
-                }
+    def update(self):
+        db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-
+    def format(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'scientific_name': self.scientific_name,
+            'is_poisonous': self.is_poisonous,
+            'primary_color': self.primary_color
+        }
